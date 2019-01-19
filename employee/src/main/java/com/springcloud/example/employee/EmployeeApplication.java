45867a657f94529ad5be38e9a062b7e427b15bdf
as <athishsreeram007@gmail.com>
2019-01-20 00:09:42 +0530

    Cloud Service get Registered with Consul with spring cloud service registry

--- employee/src/main/java/com/springcloud/example/employee/EmployeeApplication.java
+++ employee/src/main/java/com/springcloud/example/employee/EmployeeApplication.java
@@ -1,14 +1,15 @@
 package com.springcloud.example.employee;
 
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class EmployeeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeApplication.class, args);
+		new SpringApplicationBuilder(EmployeeApplication.class)
+				.run(args);
 	}
-
 }
 
